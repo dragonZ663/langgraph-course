58ef4e97d3036bca090810cfd3967f84c30a1a68
dargonZ663 <dragonz8663@gmail.com>
2026-03-23 21:51:21 +0800

    nodes

--- react.py
+++ react.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
+
 load_dotenv()
+
+import os
 
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
-import os
 
 
 @tool
@@ -15,10 +17,11 @@ def triple(num: float) -> float:
     """
     return float(num) * 3
 
+
 tools = [TavilySearch(max_results=1), triple]
 llm = ChatOpenAI(
     model="qwen/qwen3.5-9b",
     base_url=os.environ.get("LM_STUDIO_BASE_URL"),
     api_key=os.environ.get("LM_STUDIO_API_KEY"),
-    temperature=0
+    temperature=0,
 ).bind_tools(tools)
